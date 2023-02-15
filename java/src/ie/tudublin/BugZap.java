77759115f5cf2095e9350141ee9791b21367ca8a
CompSciStudent2021 <91619575+CompSciStudent2021@users.noreply.github.com>
2023-02-15 13:05:21 +0000

    BugZap

--- java/src/ie/tudublin/BugZap.java
+++ java/src/ie/tudublin/BugZap.java
@@ -3,23 +3,29 @@ package ie.tudublin;
 import processing.core.PApplet;
 
 public class BugZap extends PApplet {
+	float playerX, playerY;
+	float playerSpeed = 5;
+	float playerWidth = 50;
+	float halfPlayerWidth = playerWidth * 0.5f;
+
+	float bugWidth = 50;
+	float halfBugWidth = bugWidth * 0.5f;
+	float bugX = random(halfBugWidth, width - halfBugWidth), bugY = 50;
+
+	int score = 0;
+
 	public void settings() {
-		size(1000, 1000);
+		size(500, 500);
 	}
 
 	public void setup() {
-		reset();
+		colorMode(HSB);
+		background(0);
+		smooth();
+
+		playerX = width * 0.5f;
+		playerY = height - 100;
 	}
-
-	float playerX, playerY;
-	float playerSpeed = 5;
-	float playerWidth = 40;
-	float halfPlayerWidth = playerWidth / 2;
-
-	float bugX, bugY, bugWidth = 100;
-	float halfBugWidth = bugWidth / 2;
-
-	int score = 0;
 
 	void reset() {
 		resetBug();
@@ -38,7 +44,7 @@ public class BugZap extends PApplet {
 		float saucerHeight = bugWidth * 0.7f;
 		line(x, y - saucerHeight, x - halfBugWidth, y);
 		line(x, y - saucerHeight, x + halfBugWidth, y);
-		// line(x - halfBugWidth, y, x - halfBugWidth, y);
+		line(x - halfBugWidth, y, x - halfBugWidth, y);
 		line(x - halfBugWidth, y, x + halfBugWidth, y);
 		float feet = bugWidth * 0.1f;
 		line(x - feet, y, x - halfBugWidth, y + halfBugWidth);
@@ -55,39 +61,30 @@ public class BugZap extends PApplet {
 	}
 
 	void drawPlayer(float x, float y, float w) {
+		noFill();
 		stroke(255);
-		float playerHeight = w / 2;
-		line(x - halfPlayerWidth, y + playerHeight, x + halfPlayerWidth, y + playerHeight);
-		line(x - halfPlayerWidth, y + playerHeight, x - halfPlayerWidth, y + playerHeight * 0.5f);
-		line(x + halfPlayerWidth, y + playerHeight, x + halfPlayerWidth, y + playerHeight * 0.5f);
-
-		line(x - halfPlayerWidth, y + playerHeight * 0.5f, x - (halfPlayerWidth * 0.8f), y + playerHeight * 0.3f);
-		line(x + halfPlayerWidth, y + playerHeight * 0.5f, x + (halfPlayerWidth * 0.8f), y + playerHeight * 0.3f);
-
-		line(x - (halfPlayerWidth * 0.8f), y + playerHeight * 0.3f, x + (halfPlayerWidth * 0.8f),
-				y + playerHeight * 0.3f);
-
-		line(x, y, x, y + playerHeight * 0.3f);
-
+		rectMode(CENTER);
+		rect(x, y, w, w);
+		line(x, y - halfPlayerWidth, x, y - halfPlayerWidth * 2);
 	}
 
 	public void keyPressed() {
 		
 		if (keyCode == LEFT) {
-			if (playerX > halfPlayerWidth) {
-				playerX -= playerSpeed;
-			}
+			System.out.println("Left arrow pressed");
+			playerX--;
 		}
 		if (keyCode == RIGHT) {
-			if (playerX < width - halfPlayerWidth) {
-				playerX += playerSpeed;
-			}
+			System.out.println("Right arrow pressed");
+			playerX++;
 		}
-		if (keyCode == ' ')
+		if (key == ' ')
 		{
+			System.out.println("Space key pressed");
+
 			if (playerX > bugX - halfBugWidth && playerX < bugX + halfBugWidth)
 			{
-				line(playerX, playerY, playerX, bugY);
+				line(playerX, playerY - halfPlayerWidth, playerX, 0);
 				score ++;
 				resetBug();
 			}
@@ -115,27 +112,7 @@ public class BugZap extends PApplet {
 
 	public void draw() {
 		background(0);
-		fill(255);
-		text("Score: " + score, 50, 100);
-		if (gameMode == 0)
-		{
-			fill(255);
-			drawPlayer(playerX, playerY, playerWidth);
-			drawBug(bugX, bugY);
-			moveBug();
-
-			text("Score: " + score, 20, 20);
-		}
-		else
-		{
-			textAlign(CENTER, CENTER);
-			text("GAME OVER!!!", width / 2, height / 2);
-		}
-
-		if (bugY > height - 50)
-		{
-			gameMode = 1;
-		}
-
+		drawPlayer(playerX, playerY, playerWidth);
+		drawBug(bugX, bugY);
 	}
 }
